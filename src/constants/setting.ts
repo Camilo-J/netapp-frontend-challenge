dfb59b10568375f52108aea778ed4a5c4a541511
Camilo-J <josecamilo1902@outlook.com>
2025-02-03 16:49:31 -0500

    chore: add Pexels API keys to settings for image retrieval

--- src/constants/setting.ts
+++ src/constants/setting.ts
@@ -1,4 +1,6 @@
 const BASE_URI = import.meta.env.VITE_BASE_URI as string;
 const tokenKey = import.meta.env.VITE_TOKEN_KEY as string;
+const PEXEL_KEY = import.meta.env.VITE_PEXELS_KEY as string;
+const PEXEL_URL = import.meta.env.VITE_PEXELS_URL as string;
 
-export { BASE_URI, tokenKey };
+export { BASE_URI, tokenKey, PEXEL_KEY, PEXEL_URL };
