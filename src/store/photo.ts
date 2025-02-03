a0b1916d4b618c82dc36b8209a58c793fd41577f
Camilo-J <josecamilo1902@outlook.com>
2025-02-03 16:50:08 -0500

    feat: implement photo store state for managing photos and search terms; add photo and error types

---
+++ src/store/photo.ts
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import { Photo } from "@/types/photo";
+import { getPhotos } from "@/services/photoServices";
+
+interface PhotoState {
+  photos: Photo[];
+  searchTerm: string;
+  setPhotos: (photos: string) => Promise<void>;
+  setSearchTerm: (searchTerm: string) => void;
+}
+
+export const usePhotoStore = create<PhotoState>()((set) => ({
+  photos: [],
+  searchTerm: "",
+  setSearchTerm: (searchTerm: string) => {
+    set({ searchTerm });
+  },
+  setPhotos: async (searchTerms) => {
+    const photos = await getPhotos(searchTerms);
+    set({ photos });
+  },
+}));
