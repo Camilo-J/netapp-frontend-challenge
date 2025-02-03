8396b939198bf3cf84a19c909c2a8c4ffb08b209
Camilo-J <josecamilo1902@outlook.com>
2025-02-02 19:31:04 -0500

    feat: add user authentication types and update dependencies

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,4 +1,12 @@
+import { User } from "./user";
+
 export interface Credentials {
   email: string;
   password: string;
+}
+
+export interface LoginResponse {
+  user: User;
+  token: string;
+  refreshToken: string;
 }
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,7 +1,9 @@
 export interface User {
+  id: number;
   name: string | null;
   lastName: string;
   username: string;
+  password?: string;
   email: string;
   token?: string;
   refreshToken?: string;
